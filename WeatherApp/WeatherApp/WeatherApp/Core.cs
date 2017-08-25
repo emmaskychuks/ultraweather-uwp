dd46f5ad6a59f726f127200069998749a100dc9a
Emmanuel Obi <emmaskychuks@gmail.com>
2017-08-25 02:17:44 -0400

    Finished main aspect of the app

---
+++ WeatherApp/WeatherApp/WeatherApp/Core.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            string key = "cfd15fe2be83cbd486512d46d5c0e599";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + zipCode + "&appid=" + key;
+
+            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
